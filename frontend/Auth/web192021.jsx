f5374078a81627dbb30f0a5188dbf7dd32cd9159
Kesego Mokgosi <kesegomokgosi23@gmail.com>
2021-11-04 17:12:50 +0200

    fr

---
+++ frontend/Auth/web192021.jsx
@@ -0,0 +1,38 @@
+
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./web192021.css";
+
+class Web192021 extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+  }
+  
+  render() {
+    
+    return (
+          <div data-layer="98e513a9-a08d-4673-a5b5-3bc97818dbd3" className="web192021">        <div data-layer="7826f2a0-16ad-4179-a1d4-abb2c311750d" className="rectangle56"></div>
+        <div data-layer="28137ed1-abde-41af-b466-acabaaad1b18" className="centreConsole">            <div data-layer="7bc80e9d-4b08-406a-be3f-216a41715743" className="maskGroup1"></div>
+</div>
+</div>
+
+    );
+  }
+}
+
+Web192021.propTypes = {
+
+}
+
+Web192021.defaultProps = {
+
+}
+
+
+export default Web192021;
+          
---
+++ frontend/companiesDashboard/companiesDashboard.jsx
@@ -0,0 +1,156 @@
+
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./companiesDashboard.css";
+
+class CompaniesDashboard extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+  }
+  
+  render() {
+    
+    return (
+          <div data-layer="6f6dae84-9bd3-47b5-8c72-4881c70c132f" className="companiesDashboard">        <div data-layer="a5918875-fa3e-42c7-99a6-8966fb5e7eb0" className="screenshot142"></div>
+        <div data-layer="edbe7408-632e-471c-9177-545d4ef4f07b" className="sidePanel">            <div data-layer="1b6afcc0-c923-4bf1-8773-b9ced916ef15" className="rectangle6"></div>
+            <svg data-layer="39c18064-de9d-4d2e-959e-b4e30c40645d" preserveAspectRatio="none" viewBox="0 -0.5 119 1" className="line5"><path d="M 0 0 L 119 0"  /></svg>
+            <svg data-layer="d6ec53d9-1768-4e80-922b-40b64cbfe11e" preserveAspectRatio="none" viewBox="0 -0.5 120 1" className="line6"><path d="M 0 0 L 120 0"  /></svg>
+            <svg data-layer="247ee744-3de6-4a07-93a4-ea49bb75870f" preserveAspectRatio="none" viewBox="0 -0.5 119 1" className="line7"><path d="M 0 0 L 119 0"  /></svg>
+            <svg data-layer="b3096f85-a144-404e-a038-3271e1dbd9f2" preserveAspectRatio="none" viewBox="0 -0.5 119 1" className="line8"><path d="M 0 0 L 119 0"  /></svg>
+            <svg data-layer="2ce4f567-a40a-424d-9a34-e39d39c0a6f2" preserveAspectRatio="none" viewBox="0 -0.5 119 1" className="line9"><path d="M 0 0 L 119 0"  /></svg>
+            <svg data-layer="0b213da2-0662-4aaa-81d5-a72e5c4d9152" preserveAspectRatio="none" viewBox="0 -0.5 119 1" className="line10"><path d="M 0 0 L 119 0"  /></svg>
+            <div data-layer="fc4fa471-b1e6-44be-ad1c-9b1f0e5706dc" className="rectangle50"></div>
+            <div data-layer="57ffd5a7-2cc0-4914-8afa-e66ce3b517df" className="dashab3317f0">                <div data-layer="5bee26dc-abc3-419a-b12b-59a1439a062a" className="ourInterns">Our Interns</div>
+                <div data-layer="2f9a0d52-55f3-43e1-98a3-42dfa1df3eed" className="administratorIcon"></div>
+</div>
+            <div data-layer="76a9f94f-9306-4221-85e6-a7b1e25e89e9" className="manage693fae4c">                <div data-layer="d9e755b0-6deb-46c6-969e-c56695dc046c" className="compliancecentre">Compliance<br />Centre</div>
+                <div data-layer="c3c30587-80de-4000-a758-06384a62de53" className="conferenceIcond4a69856"></div>
+</div>
+            <div data-layer="cb19fcff-3fdd-4717-9c46-17c4cc4371a2" className="manage22d1e1e2">                <div data-layer="734a2ff8-3147-4003-aa54-ece111397a9e" className="applications">Applications</div>
+                <div data-layer="642f0fee-02fb-436c-8550-853d51656c4f" className="conferenceIcon"></div>
+</div>
+            <div data-layer="83bd7b30-9f3f-442f-b074-7508be739614" className="manage">                <div data-layer="814abdb7-6aa6-4af9-afe0-2f3790d1a392" className="openVacancies">Open <br />Vacancies</div>
+                <div data-layer="890f0003-4b0e-40e0-9844-2076b37d5b99" className="libraryIcon"></div>
+</div>
+            <div data-layer="a32741f9-cb49-4169-80c5-164b3b22d0d4" className="dash">                <div data-layer="ad8315b4-ec42-4986-a690-648d0fa2149b" className="dashboard">Dashboard</div>
+                <div data-layer="89b2cdf7-e738-497c-9deb-6daa3570a232" className="dashboardIcon"></div>
+</div>
+</div>
+        <div data-layer="7a904a2b-cee3-4055-bc52-c44f3364b724" className="group8">            <div data-layer="da4308ae-1cb9-4ff2-83d0-c5f5c080e152" className="panel"></div>
+            <div data-layer="c2798cf6-c62c-4c64-be8c-768eea5c3f83" className="logo"></div>
+</div>
+        <div data-layer="77f17bba-a464-4ad6-92d9-a3a8fbdc1ea0" className="topNaming">            <div data-layer="1322c782-9e97-47d8-8f4c-1d598ff9d69b" className="group33">                <div data-layer="3abecb61-1202-4c48-af11-a01bb748345b" className="bryteInsurance"><span className="bryteInsurance-0">Bryte </span><span className="bryteInsurance-6">Insurance</span></div>
+</div>
+            <div data-layer="2d5f0db2-6ea0-4052-b65a-f048b916e3de" className="image14"></div>
+</div>
+        <div data-layer="d9b4b756-e529-4095-b099-4527ca105048" className="syncData">            <div data-layer="08933aa8-695f-4e03-b1f9-0436eacf6162" className="rectangle68"></div>
+            <div data-layer="2635cbb8-f227-4a3b-a4d5-0d898cfbf0b9" className="realtime">Realtime</div>
+            <div data-layer="bdf9ef8e-b95a-46ae-832e-c34bd2a0c66b" className="lastSynced2MinutesAgo">Last synced 2 minutes ago</div>
+            <div data-layer="4232b5f2-0347-42c7-a599-17c6e1a06daf" className="group87">                <div data-layer="7b2d61df-017d-45ec-8acb-3d5c2971151d" className="rectangle69"></div>
+                <div data-layer="35d5dcee-4cf9-4687-a47b-0c5f0d0ec9f3" className="refreshData">Refresh Data</div>
+                <div data-layer="576b67d9-cb2d-4def-9898-95d605fadd57" className="mdiSyncCircle15e65de1">                    <div data-layer="22561ec7-4ca1-4073-b1ec-f0a777cb7e91" className="boundary1bff7ed9"></div>
+                    <svg data-layer="c313b81f-1995-485b-bccc-c88cedc7423e" preserveAspectRatio="none" viewBox="1.2499990463256836 1.25 32.1962890625 32.19677734375" className="pathSyncCirclef9dd3400"><path d="M 2 17.34830665588379 C 2 25.82494354248047 8.871671676635742 32.69661331176758 17.34830665588379 32.69661331176758 C 25.82494354248047 32.69661331176758 32.69661331176758 25.8249397277832 32.69661331176758 17.34830665588379 C 32.69661331176758 8.871670722961426 25.8249397277832 2 17.34830665588379 2 C 8.871670722961426 2 1.999998450279236 8.871673583984375 2 17.34830856323242 M 22.87369728088379 19.98821449279785 C 23.2724552154541 19.16497039794922 23.48221015930176 18.26302337646484 23.48762893676758 17.34830665588379 C 23.48762893676758 13.95765209197998 20.73896026611328 11.208984375 17.34830665588379 11.208984375 L 17.34830665588379 14.27864551544189 L 12.55963516235352 9.674153327941895 L 17.34830665588379 5.069661617279053 L 17.34830665588379 8.139322280883789 C 22.43428802490234 8.139322280883789 26.55729103088379 12.26232624053955 26.55729103088379 17.34830665588379 C 26.56038284301758 19.08338928222656 26.06493377685547 20.78283309936523 25.1298999786377 22.24441719055176 M 8.139322280883789 17.34830665588379 C 8.136232376098633 15.6132287979126 8.631681442260742 13.91378402709961 9.566712379455566 12.4522008895874 L 11.82291603088379 14.70839786529541 C 11.42416000366211 15.53164100646973 11.21440410614014 16.43358612060547 11.208984375 17.34830284118652 C 11.208984375 20.73896217346191 13.95765590667725 23.48763275146484 17.34831047058105 23.48762893676758 L 17.34830665588379 20.41796684265137 L 21.95279884338379 25.02245903015137 L 17.34830665588379 29.62695121765137 L 17.34830665588379 26.55729103088379 C 12.26232433319092 26.55729103088379 8.139322280883789 22.43428611755371 8.139322280883789 17.34830665588379 Z"  /></svg>
+</div>
+</div>
+            <div data-layer="ac9a5d9a-0a3f-46ee-bec4-5e153c470bc0" className="mdiSyncCircle">                <div data-layer="1b6cbe04-db8d-4194-b3d6-4ab1ea4b1f30" className="boundary"></div>
+                <svg data-layer="2a90a8c5-8766-488e-a642-252e2f89a33a" preserveAspectRatio="none" viewBox="1.25 1.25 34.939453125 34.93994140625" className="pathSyncCircle"><path d="M 2 18.719970703125 C 2 27.95415687561035 9.485786437988281 35.43994140625 18.719970703125 35.43994140625 C 27.95415687561035 35.43994140625 35.43994140625 27.95415306091309 35.43994140625 18.719970703125 C 35.43994140625 9.485785484313965 27.95415306091309 2 18.719970703125 2 C 9.485785484313965 2 1.999998331069946 9.485788345336914 2 18.71997451782227 M 24.73916053771973 21.59580612182617 C 25.17355537414551 20.69898796081543 25.40205574035645 19.7164363861084 25.407958984375 18.719970703125 C 25.407958984375 15.02629661560059 22.41364479064941 12.031982421875 18.719970703125 12.031982421875 L 18.719970703125 15.3759765625 L 13.50333976745605 10.3599853515625 L 18.719970703125 5.343994140625 L 18.719970703125 8.68798828125 C 24.26048278808594 8.68798828125 28.751953125 13.1794605255127 28.751953125 18.719970703125 C 28.75532150268555 20.61011695861816 28.21559524536133 22.4614372253418 27.19699859619141 24.05364227294922 M 8.68798828125 18.719970703125 C 8.684621810913086 16.82983016967773 9.224348068237305 14.97850799560547 10.24294281005859 13.38630390167236 L 12.70078086853027 15.84413528442383 C 12.26638793945312 16.74095153808594 12.03788661956787 17.72350311279297 12.031982421875 18.71996688842773 C 12.031982421875 22.41364669799805 15.02630043029785 25.40796279907227 18.7199764251709 25.407958984375 L 18.719970703125 22.06396484375 L 23.7359619140625 27.0799560546875 L 18.719970703125 32.095947265625 L 18.719970703125 28.751953125 C 13.17945861816406 28.751953125 8.68798828125 24.2604808807373 8.68798828125 18.719970703125 Z"  /></svg>
+</div>
+            <div data-layer="b6817db2-9769-450b-968a-f5cd2403392c" className="group95">                <div data-layer="e94a448e-27d0-414a-a5a5-1e0f9045b2b7" className="rectangle70382e6313"></div>
+                <div data-layer="89463945-e573-46bf-a898-ec36dfaef029" className="maleInterns">Male Interns </div>
+                <div data-layer="c754b196-8d3e-4c44-83c6-dcd3bbce549e" className="x8">8</div>
+                <svg data-layer="02cab3b0-a14e-4f78-bca2-bde0d6d3f675" preserveAspectRatio="none" viewBox="3.375 3.375 29.25 29.25" className="iconIonicMdMale"><path d="M 29.25 3.375 L 20.25 3.375 L 20.25 6.75 L 26.86640548706055 6.75 L 19.36406326293945 14.24531269073486 C 17.71171951293945 13.07109355926514 15.68671894073486 12.375 13.5 12.375 C 7.91015625 12.375 3.375 16.91015625 3.375 22.5 C 3.375 28.08984375 7.91015625 32.625 13.5 32.625 C 19.08984375 32.625 23.625 28.08984375 23.625 22.5 C 23.625 20.31328201293945 22.92890548706055 18.28828048706055 21.75468826293945 16.63593673706055 L 29.25 9.133593559265137 L 29.25 15.75 L 32.625 15.75 L 32.625 6.75 L 32.625 3.375 L 29.25 3.375 Z M 13.5 29.25 C 9.780468940734863 29.25 6.75 26.21953201293945 6.75 22.5 C 6.75 18.78046798706055 9.780468940734863 15.75 13.5 15.75 C 17.21953201293945 15.75 20.25 18.78046798706055 20.25 22.5 C 20.25 26.21953201293945 17.21953201293945 29.25 13.5 29.25 Z"  /></svg>
+</div>
+            <div data-layer="2e4eef49-f1f0-4d4f-bd0e-f4e212309583" className="group103">                <div data-layer="b3716982-201b-4187-b962-e002668cc624" className="rectangle70"></div>
+                <div data-layer="1f7e1b13-de31-48a8-9d4e-454160d5ca40" className="femaleInterns">Female Interns</div>
+                <div data-layer="b876eb8e-9699-4a60-a6b3-220de28bebdb" className="x9">9</div>
+                <svg data-layer="778a51fd-ad26-4f54-931e-86f0918efc83" preserveAspectRatio="none" viewBox="7.875 2.25 20.25 31.5" className="iconIonicMdFemale"><path d="M 28.125 12.375 C 28.125 6.78515625 23.58984375 2.25 18 2.25 C 12.41015625 2.25 7.875 6.78515625 7.875 12.375 C 7.875 17.39531326293945 11.52421855926514 21.55781173706055 16.3125 22.359375 L 16.3125 25.875 L 11.25 25.875 L 11.25 29.25 L 16.3125 29.25 L 16.3125 33.75 L 19.6875 33.75 L 19.6875 29.25 L 24.75 29.25 L 24.75 25.875 L 19.6875 25.875 L 19.6875 22.359375 C 24.47578048706055 21.55781173706055 28.125 17.39531326293945 28.125 12.375 Z M 11.25 12.375 C 11.25 8.655468940734863 14.28046894073486 5.625 18 5.625 C 21.71953201293945 5.625 24.75 8.655468940734863 24.75 12.375 C 24.75 16.09453201293945 21.71953201293945 19.125 18 19.125 C 14.28046798706055 19.125 11.25 16.09453201293945 11.25 12.375 Z"  /></svg>
+</div>
+</div>
+        <div data-layer="21de38b5-08c9-4712-a0ff-6fca3c849135" className="profitAndLoss4076f39f">            <div data-layer="97d851cd-d3a3-4ada-8695-bef8170642c4" className="rectangle88d01c14af"></div>
+            <div data-layer="78b49b67-b5ee-4086-8907-fe48388c73ee" className="rectangle4610d716e82"></div>
+            <div data-layer="7cb1d2b3-459a-49c3-b38a-79397e871ecf" className="maleStatistics">Male Statistics</div>
+            <div data-layer="125ea790-dcdf-49c3-b1b2-c84ad1baf1d3" className="overallPerformance889bbf7b">Overall Performance</div>
+            <div data-layer="9e9403ba-9208-42c9-81f3-4c4ef44a2fcf" className="xdb3a4e26">-</div>
+            <div data-layer="c4689e83-349e-4559-b59a-58474efab352" className="x65">65%</div>
+            <div data-layer="259500b6-304d-4fc8-8c75-83bc8ca83874" className="group2995d6ca873a">                <div data-layer="50ceec51-af7b-4d42-bc05-755f0647aed9" className="rectangle71eeff2bfd"></div>
+                <div data-layer="18be0efb-aeb6-4431-9788-36d3e3faaab2" className="mar2020Feb2021f2d7492f">Mar 2020 - Feb 2021</div>
+</div>
+            <div data-layer="eb805242-948c-47a5-a650-37509fd13a0d" className="punctualityae602ad1">Punctuality</div>
+            <div data-layer="e71c988a-2aad-4c66-b7bb-2373f495a856" className="timeManagement7d6fea28">Time Management</div>
+            <div data-layer="487b123a-4305-43b7-8765-e33412603396" className="communicationSkills8aa0bef0">Communication Skills</div>
+            <div data-layer="05ff3c2b-cba1-4c57-b73a-5f5bb8b55509" className="appearance308b2ad8">Appearance</div>
+            <div data-layer="52aa1646-252e-451a-974c-6798243e637d" className="teamWork13ec3f9d">Team Work</div>
+            <div data-layer="9cb69940-a685-48e3-b041-f7f239cdce93" className="rectangle89c6990084"></div>
+            <div data-layer="518cefaf-5876-4835-b03d-092526aae6e8" className="rectangle45572a952d8"></div>
+            <div data-layer="d9aee86f-7a2d-4ec8-b05d-9989759d05bf" className="rectangle456611db577"></div>
+            <div data-layer="25871e37-9e31-4e9d-a231-7b55b6c37a2b" className="rectangle457fb4c0648"></div>
+            <div data-layer="3e324f00-ea36-4982-bbaa-8dee16d297c8" className="rectangle4588d6de897"></div>
+            <div data-layer="984e3eb4-2330-4cbd-99c7-5dd0f824033b" className="rectangle73bdbe6395"></div>
+            <div data-layer="1a934592-c76f-495a-8c68-f1521c143cb8" className="rectangle74e1522752"></div>
+            <div data-layer="fcc327a1-7392-4826-b7c9-92c0413ba677" className="rectangle763763428a"></div>
+            <div data-layer="0f6e22c1-c015-49bc-a984-5e551f07b851" className="rectangle454ff878bd2"></div>
+            <div data-layer="b5ef653d-8af8-4764-8b4c-15e1f4065a7f" className="rectangle459b35607c4"></div>
+            <div data-layer="907a66c3-e070-4bfc-8a1f-cc4dfa8b1651" className="x45b6b8fe4d">45%</div>
+            <div data-layer="1bd34c0a-cd59-4cc9-877e-e317505150ea" className="x95">95%</div>
+            <div data-layer="b06f1f22-b5d4-4b88-91bd-2a93c664454b" className="x358fa7b4aa">35%</div>
+            <div data-layer="a7efbbde-6495-4843-8c27-8dedb716fbf2" className="x75badf8260">75%</div>
+            <div data-layer="8e5d4636-328e-4407-886b-37010b086446" className="x3513">35.13%</div>
+            <div data-layer="11faa011-1677-4131-bd2c-0ee3e1a9714f" className="group2996075504ac">                <div data-layer="8bc94702-f6bf-4dd3-8391-8ff9a4fed18b" className="rectangle85ddfb3aa9"></div>
+                <div data-layer="a8c71707-c617-4efc-96eb-12d4d3af681d" className="more7064a4c9">More</div>
+</div>
+</div>
+        <div data-layer="a201e12c-72e4-4892-b152-4cb1dbeaa368" className="profitAndLoss">            <div data-layer="3b94f1e8-068c-4391-a789-3f05065140fd" className="rectangle88"></div>
+            <div data-layer="fb39be97-b1ee-4933-bf75-3e6448b7252e" className="rectangle461"></div>
+            <div data-layer="77d14cc7-aeba-468e-a03f-13ea55bd09f1" className="femaleStatistics">Female Statistics</div>
+            <div data-layer="4494cb10-48b2-4865-b935-e1c3d898d319" className="overallPerformance">Overall Performance</div>
+            <div data-layer="05778882-7632-4eef-9793-fe94a0c3045b" className="x">-</div>
+            <div data-layer="0f4dc992-9711-46bf-b238-4c3740855494" className="x76">76%</div>
+            <div data-layer="ac0f802d-6665-45c3-bf33-98f4db69cb2c" className="group2995">                <div data-layer="d77d1724-6d5f-4cc1-be3f-4e744d0ac92a" className="rectangle71"></div>
+                <div data-layer="90f2df80-0427-42b8-b7ed-38459ef6ac5f" className="mar2020Feb2021">Mar 2020 - Feb 2021</div>
+</div>
+            <div data-layer="67d7eb5a-a4a3-46ec-a6d5-ef4f02b4aedb" className="punctuality">Punctuality</div>
+            <div data-layer="d7a2c01a-2dd3-48ed-99e0-b32247a9d5d4" className="timeManagement">Time Management</div>
+            <div data-layer="79f36ef8-9847-42d8-931d-c81c58c4e098" className="communicationSkills">Communication Skills</div>
+            <div data-layer="0fabce44-e19a-492f-b4a6-844da9eeca92" className="appearance">Appearance</div>
+            <div data-layer="af78485a-54c9-4feb-bf5a-32eb9cb05cb3" className="teamWork">Team Work</div>
+            <div data-layer="b2b62dce-4515-47f0-854b-fdbdae79fbde" className="rectangle89"></div>
+            <div data-layer="f6c6573f-5ef6-4488-a90b-52eda3c9ff5f" className="rectangle455"></div>
+            <div data-layer="e34d2d80-897c-466e-b0d5-7618ff703ce6" className="rectangle456"></div>
+            <div data-layer="d25787d4-fa40-44e2-8827-4610d44eccdf" className="rectangle457"></div>
+            <div data-layer="6f55a7a1-6c99-44d6-8c9a-8652617a8d27" className="rectangle458"></div>
+            <div data-layer="5ed2b6b9-ae32-46cb-aa2d-56edc85c7501" className="rectangle73"></div>
+            <div data-layer="44aa2f4f-9684-446a-8ba1-b5465d2ceb11" className="rectangle74"></div>
+            <div data-layer="373c8561-74db-4dfe-b724-45a578d4f4a2" className="rectangle76"></div>
+            <div data-layer="5897b18b-49d8-4eaf-be85-0df50fb23934" className="rectangle454"></div>
+            <div data-layer="c79e201e-d486-47d2-8fac-35356bcce9e1" className="rectangle459"></div>
+            <div data-layer="242185b0-a82f-4089-8b02-3721c01da1c9" className="x6701">67.01%</div>
+            <div data-layer="60ba58d3-8761-494b-8c0f-fdaf63805386" className="x45">45%</div>
+            <div data-layer="14a41aa6-b350-4541-acf4-a306d2f1f706" className="x35">35%</div>
+            <div data-layer="6569a231-7842-4fd7-98a7-66d885897b8a" className="x75">75%</div>
+            <div data-layer="563a364d-8d7f-4e36-9726-d8927f4e3a5b" className="x50">50%</div>
+            <div data-layer="065db44e-fcf3-4e40-8940-25605f2f82ec" className="group2996">                <div data-layer="246af3ea-f23c-4512-a5c3-e786f7faf200" className="rectangle85"></div>
+                <div data-layer="1af1671d-f3f7-4bbb-acc5-9be0f9838c9a" className="more">More</div>
+</div>
+</div>
+</div>
+
+    );
+  }
+}
+
+CompaniesDashboard.propTypes = {
+
+}
+
+CompaniesDashboard.defaultProps = {
+
+}
+
+
+export default CompaniesDashboard;
+          
