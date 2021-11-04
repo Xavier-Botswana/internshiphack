18fd23722b1c3e8b8369368515087cdb80e78653
Kesego Mokgosi <kesegomokgosi23@gmail.com>
2021-11-04 09:11:32 +0200

    models

---
+++ models/try.py
@@ -0,0 +1,44 @@
+#importing python packages that are going to be used
+import pandas as pd
+import numpy as np
+import re
+import sklearn
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("")#reading the dataset that is going to be used by the model
+print(df.info())
+#Looking unique values
+print(df.nunique())
+#Looking the data
+print(df.head())
+df.replace(to_replace ="good", 
+                            value =1) 
+include = ['Age', 'Sex', 'Job', 'interests','kyc','competencies','qualifications','Duration','experiencece','period'] # selection of features
+df_ = df[include]
+categoricals = []
+for col, col_type in df_.dtypes.iteritems():
+     if col_type == 'O':
+          categoricals.append(col)
+     else:
+          df_[col].fillna(0, inplace=True)
+
+df_ohe = pd.get_dummies(df_, columns=categoricals, dummy_na=True)
+
+from sklearn.linear_model import LogisticRegression # importing scikit-learn's Logistic Regression
+dependent_variable = 'outcome'
+x = df_ohe[df_ohe.columns.difference([dependent_variable])]
+y = df_ohe[dependent_variable]
+lr = LogisticRegression()
+lr.fit(x, y) #fitting of the model
+
+from sklearn.externals import joblib
+joblib.dump(lr, 'model.pkl')#saving the model
+
+# Load the model that  just saved
+lr = joblib.load('model.pkl')
+
+# Saving the data columns from training
+model_columns = list(x.columns)
+joblib.dump(model_columns, 'model_columns.pkl')
+print("Models columns dumped!")
